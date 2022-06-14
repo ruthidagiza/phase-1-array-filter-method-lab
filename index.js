0e821d4dfdbbdd23c2c8e7d49ba27d29f2cf1dc2
IDAGIZA <ruthidagiza@gmail.com>
2022-06-14 20:56:52 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+    function findMatching (array,name) {
+        return array.filter(function(driverName){
+            return driverName.toLowerCase() === name.toLowerCase()
+        })
+    }
+
+    function fuzzyMatch(array,name){
+        return array.filter(function(driverName){
+            return driverName.toLowerCase().startsWith(name.toLowerCase())
+        })
+
+    }
+    const drivers2 = [
+        {
+          name: 'Bobby',
+          hometown: 'Pittsburgh' },
+        {
+          name: 'Sammy',
+          hometown: 'New York' } ,
+        {
+          name: 'Sally',
+          hometown: 'Cleveland' },
+        {
+          name: 'Annette',
+          hometown: 'Los Angeles' },
+        {
+          name: 'Bobby',
+          hometown: 'Tampa Bay' }
+      ];
+
+    function matchName(array,name){
+        return array.filter(function(driver){
+            return driver.name.toLowerCase() === name.toLowerCase()
+        })
+
+    }
+
+
+    console.log(findMatching(drivers, "Bobby"))
+    console.log(fuzzyMatch(drivers, "S"))
+    console.log(matchName(drivers2,"Annette"))
